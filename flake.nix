5c5a74fb6a53d8321c4a0cc6b97f91dbd79b3305
thedavidmeister <thedavidmeister@gmail.com>
2024-02-25 22:54:41 +0400

    debug

--- flake.nix
+++ flake.nix
@@ -71,7 +71,7 @@
 
               cp ${pkgs.gettext}/lib/libintl.8.dylib lib/libintl.8.dylib
               chmod +w lib/libintl.8.dylib
-              install_name_tool -id ${pkgs.gettext}/lib/libintl.8.dylib @executable_path/../Frameworks/libintl.8.dylib lib/libintl.8.dylib
+              install_name_tool -id @executable_path/../Frameworks/libintl.8.dylib lib/libintl.8.dylib
             '';
           };
         } // rainix.packages.${system};
